0d74757a285b759136d8b0f0ff9391a6d1ca45de
Peacheyes <saint1131@naver.com>
2026-06-07 16:55:32 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import AdminPage from './pages/AdminPage';
 
-function App() {g
+function App() {
   return (
     <BrowserRouter>
       <Routes>
